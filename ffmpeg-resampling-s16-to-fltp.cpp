023dcfda162abfbc8caed0eaafe2a00a50310bd7
Kousik Satish <kousiksatish@gmail.com>
2020-10-28 11:20:22 +0530

    Add code for resampling to flt format

--- ffmpeg-resampling-s16-to-fltp.cpp
+++ ffmpeg-resampling-s16-to-fltp.cpp
@@ -18,19 +18,51 @@ int main(int argc, char** argv) {
     
     int64_t dst_ch_layout = AV_CH_LAYOUT_STEREO;
     int dst_rate = 44100;
-    enum AVSampleFormat dst_sample_fmt = AV_SAMPLE_FMT_FLTP;
+    enum AVSampleFormat dst_sample_fmt = AV_SAMPLE_FMT_FLT;
 
     int ret;
 
     int src_nb_channels = 0;
     int src_linesize;
     uint8_t **src_data = NULL;
-    int src_nb_samples = 20; // Vary based on number of samples in input
+    int src_nb_samples = 7000*32; // Vary based on number of samples in input
 
     int dst_nb_samples, max_dst_nb_samples;
     int dst_nb_channels = 0;
     int dst_linesize;
     uint8_t **dst_data = NULL;
+
+    char* src_filename = NULL;
+    FILE *src_file;
+    int src_bufsize;
+
+    char* dst_filename = NULL;
+    FILE *dst_file;
+    int dst_bufsize;
+
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s input_file output_file\n"
+                "API example program to show how to resample an audio stream with libswresample.\n"
+                "This program generates a series of audio frames, resamples them to a specified "
+                "output format and rate and saves them to an output file named output_file.\n",
+            argv[0]);
+        exit(1);
+    }
+
+    src_filename = argv[1];
+    src_file = fopen(src_filename, "r");
+    if (!src_file) {
+        fprintf(stderr, "Could not open source file %s\n", src_filename);
+        exit(1);
+    }
+
+    dst_filename = argv[2];
+
+    dst_file = fopen(dst_filename, "wb");
+    if (!dst_file) {
+        fprintf(stderr, "Could not open destination file %s\n", dst_filename);
+        exit(1);
+    }
 
     /* create resampler context */
     swr_ctx = swr_alloc();
@@ -67,4 +99,62 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Could not allocate source samples\n");
         return -1;
     }
+
+    printf("Line size : %d\n", src_linesize);
+
+    fread(src_data[0], 1, src_linesize, src_file);
+
+    /* compute the number of converted samples: buffering is avoided
+     * ensuring that the output buffer will contain at least all the
+     * converted input samples */
+    max_dst_nb_samples = dst_nb_samples =
+        av_rescale_rnd(src_nb_samples, dst_rate, src_rate, AV_ROUND_UP);
+
+    printf("Number of samples in dest: %d\n", dst_nb_samples);
+
+    /* buffer is going to be directly written to a rawaudio file, no alignment */
+    dst_nb_channels = av_get_channel_layout_nb_channels(dst_ch_layout);
+    ret = av_samples_alloc_array_and_samples(&dst_data, &dst_linesize, dst_nb_channels,
+                                             dst_nb_samples, dst_sample_fmt, 0);
+    if (ret < 0) {
+        fprintf(stderr, "Could not allocate destination samples\n");
+        return -1;
+    }
+
+    ret = swr_convert(swr_ctx, dst_data, dst_nb_samples, (const uint8_t **)src_data, src_nb_samples);
+    printf("Number of samples converted %d\nDest Line size %d\n", ret, dst_linesize);
+    if (ret < 0) {
+        fprintf(stderr, "Error while converting\n");
+        return -1;
+    }
+
+    dst_bufsize = av_samples_get_buffer_size(&dst_linesize, dst_nb_channels, ret, dst_sample_fmt, 1);
+
+    if (dst_bufsize < 0) {
+        fprintf(stderr, "Could not get sample buffer size\n");
+        return -1;
+    }
+    
+    printf("in:%d out:%d bufSize:%d\n", src_nb_samples, ret, dst_bufsize);
+
+    fwrite(dst_data[0], 1, dst_bufsize, dst_file);
+
+    fprintf(stderr, "Resampling succeeded. Play the output file with the command:\n"
+        "aplay --format=FLOAT_LE -r %d -c %d %s\n",
+        dst_rate, dst_nb_channels, dst_filename);
+
+    fclose(src_file);
+    fclose(dst_file);
+    
+    if (src_data)
+        av_freep(&src_data[0]);
+    av_freep(&src_data);
+
+    if (dst_data)
+        av_freep(&dst_data[0]);
+    av_freep(&dst_data);
+
+    swr_free(&swr_ctx);
+
+    return 0;
 }
